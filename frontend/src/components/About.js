b7ba464c359f555de59bc3b160e8eb64fee570bf
subrataroy321 <subrata.r321@gmail.com>
2020-09-03 18:07:18 -0400

    [About] add about page

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h1> About </h1>
+            <p>This is all about us.</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quaerat ipsum fuga distinctio laboriosam. Maiores nostrum, ratione adipisci dolore quasi quas deserunt veritatis asperiores modi, aliquid laudantium officiis, architecto repellat?</p>
+        </div>
+    )
+}
+
+export default About;
