2dbf15c9b83ac1d6a376a3620bd20d5453edfb9b
subrataroy321 <subrata.r321@gmail.com>
2020-09-03 12:43:43 -0400

    initialize node and install node packages

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+const passport = require('passport');
+
+// middleware
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// passport middleware
+app.use(passport.initialize());
+//inporting passport file into server
+require('./config/passport')(passport);
+
+
+// <<<<<<< SERVER HOME ROUTE >>>>>>>>>>
+app.get('/', (req,res)=> {
+    res.status(200).json({message: 'Smile, You are being watched by the Backend Team.'});
+});
+
+app.listen(port, () => {
+    console.log(`You are listening smooth to port ${port}`);
+});
