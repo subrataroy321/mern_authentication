0651fadcd23efabb646e631d8cfafeeb2f53b010
subrataroy321 <subrata.r321@gmail.com>
2020-09-03 16:58:23 -0400

    [users.js] add user /login route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport')
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// load user model
+// load models
 const db = require('../../models')
 
 // GET api/users/test route
@@ -31,39 +31,81 @@ router.get('/', (req,res)=> {
 // POST api/users/register (Public)
 router.post('/register', (req, res) => {
   
-    // Find user by email
-    db.User.findOne({ email: req.body.email })
-    .then(user => {
-      // if email already exists, send a 400 response
-      if (user) {
-        return res.status(400).json({ msg: 'Email already exists'});
-      } else {
-        // Create a new user
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
+  // Find user by email
+  db.User.findOne({ email: req.body.email })
+  .then(user => {
+    // if email already exists, send a 400 response
+    if (user) {
+      return res.status(400).json({ msg: 'Email already exists'});
+    } else {
+      // Create a new user
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // Salt and hash the password, then save the user
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if (error) throw error;
+          // Change the password to the hash
+          newUser.password = hash;
+          newUser.save()
+          .then(createdUser => res.json(createdUser))
+          .catch(error =>  console.log(error));
         });
-  
-        // Salt and hash the password, then save the user
-        bcrypt.genSalt(10, (error, salt) => {
-          bcrypt.hash(newUser.password, salt, (error, hash) => {
-            if (error) throw error;
-            // Change the password to the hash
-            newUser.password = hash;
-            newUser.save()
-            .then(createdUser => res.json(createdUser))
-            .catch(error =>  console.log(error));
+      });
+    }
+  })
+  .catch(err => {
+      console.log('Error while creating a user ',err)
+      res.status(503).send({message: 'Server Error'})
+      
+  })
+    
+});
+
+router.post('/login', (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user using email
+  db.User.findOne({email})
+  .then(user => {
+
+    if (!user) {
+      res.status(400).json({ message: 'User not found'});
+    } else {
+      // check password with bcrypt
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+
+        if (isMatch) {
+          // user match send JSON web token
+          // create a token payload 
+
+          const payload = {
+            id: user.id,
+            name: user.name,
+            email: user.email
+          };
+
+          // sign token
+          jwt.sign(payload, JWT_SECRET, {expiresIn: 3600}, (error,token) => {
+            res.json({ success: true, token: `Bearer ${token}`});
           });
-        });
-      }
-    })
-    .catch(err => {
-        console.log('Error while creating a user ',err)
-        res.status(503).send({message: 'Server Error'})
-        
-    })
+        } else {
+          return res.status(400).json({ password: 'Password or email is incorrect'})
+        }
+
+      })
+
+    }
     
   });
+
+});
+
 
 module.exports = router;
