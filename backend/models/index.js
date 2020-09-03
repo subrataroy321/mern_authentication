aa6b6faa408a640a3b494d404446bb9f00075f89
subrataroy321 <subrata.r321@gmail.com>
2020-09-03 13:30:28 -0400

    [models/index.js] set up mongodb connection

---
+++ backend/models/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+// mongo connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+
+// mongoose connection object
+const db = mongoose.connection;
+
+
+db.once('open', () => {
+    console.log(`Connected to MongoDB ${d.host}:${db.port}`)
+})
+
+db.on('error', error => {
+    console.log(`Database error\n${error}`)
+})
+
+module.exports.User = require('./User')
