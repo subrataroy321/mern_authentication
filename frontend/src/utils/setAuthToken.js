5a258dd61a7bdcf49343b32283f930f2f309cbdc
subrataroy321 <subrata.r321@gmail.com>
2020-09-03 17:43:15 -0400

    [/src/utils/setAuthToken.js] setup token in header

---
+++ frontend/src/utils/setAuthToken.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios';
+
+// This utility will add the authorised user's JWT to request header
+// Any routes that are protected will require the JWT in order to access token
+
+const setAuthToken = token => {
+    if(token) {
+        // Apply the token to every request header
+        axios.defaults.headers.common['Authorization'] = token;
+        console.log(axios.defaults.headers.common);
+    } else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+};
+
+export default setAuthToken;
